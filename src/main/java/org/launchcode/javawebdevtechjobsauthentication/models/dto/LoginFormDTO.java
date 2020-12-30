e078acbd36d5cef5534054b8e70b50a79aacdbfc
Russell <rpsanten@gmail.com>
2020-12-30 15:55:32 -0600

    ehh

---
+++ src/main/java/org/launchcode/javawebdevtechjobsauthentication/models/dto/LoginFormDTO.java
@@ -0,0 +1,37 @@
+package org.launchcode.javawebdevtechjobsauthentication.models.dto;
+
+import com.sun.istack.NotNull;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+public class LoginFormDTO {
+
+    @NotNull
+    @NotBlank
+    @Size(min = 3, max = 30, message = "Username not acceptable. Please enter a username between" +
+            " 3 and 30 characters.")
+    private String username;
+
+    @NotNull
+    @NotBlank
+    @Size(min = 4, max = 27, message="Invalid passowrd. Please enter a new password that is between" +
+            " 4 and 27 characters")
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername() {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
